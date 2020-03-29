a419608944ebaa220a014defe757d676d3402f1a
italo-lima <italojonas@hotmail.com>
2020-03-29 15:55:33 -0300

    backend validation using celebrate

--- BeTheHero/backend/src/routes.js
+++ BeTheHero/backend/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const {celebrate, Joi, Segments} = require('celebrate')
 
 const OngController = require('./controller/OngController')
 const IncidentController = require('./controller/IncidentController')
@@ -10,12 +11,33 @@ const routes = express.Router()
 routes.post('/session', SessionController.create)
 
 routes.get('/ongs', OngController.index)
-routes.post('/ongs', OngController.create)
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().email(),
+        whatsapp: Joi.number().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().length(2)
+    })
+}) ,OngController.create)
 
 routes.get('/incidents', IncidentController.index)
-routes.post('/incidents', IncidentController.create)
-routes.delete('/incidents/:id', IncidentController.delete)
+routes.post('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number()
+    })
+}),IncidentController.create)
 
-routes.get('/profile-incidents', ProfileController.index)
+routes.delete('/incidents/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required()
+    })
+}),IncidentController.delete)
+
+routes.get('/profile-incidents', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required()
+    }).unknown()
+}),ProfileController.index)
 
 module.exports = routes
